3308805116bc88c04c75282440416cd24b8193e6
JS Soluções e Serviços <jssolucoeseservicos@gmail.com>
2024-08-07 14:01:37 -0300

    FIX add link

--- src/app/scan/[id]/get-link.ts
+++ src/app/scan/[id]/get-link.ts
@@ -1,23 +1,27 @@
-'use server'
- 
+"use server";
+
 export async function getLink(id: string) {
   let link = null;
   try {
-    if ( id === 'igreja-viva-banners-igreja') {
-        //link = `https://recepcao.igrejavivaemcelulas.com.br/pages/a-igreja`;
-        link = `https://linktr.ee/igreja.viva`;
-      }
-    
-      if ( id === 'igreja-viva-banners-viva') {
-        //link = `https://recepcao.igrejavivaemcelulas.com.br/pages/social`;
-        link = `https://linktr.ee/igreja.viva`;
-      }
-    
-      if ( id === 'igreja-viva-banners-celula') {
-        //link = `https://recepcao.igrejavivaemcelulas.com.br/pages/celulas`;
-        link = `https://linktr.ee/igreja.viva`;
-      }
-      return link;
+    if (id === "igreja-viva-banners-igreja") {
+      //link = `https://recepcao.igrejavivaemcelulas.com.br/pages/a-igreja`;
+      link = `https://linktr.ee/igreja.viva`;
+    }
+
+    if (id === "igreja-viva-banners-viva") {
+      //link = `https://recepcao.igrejavivaemcelulas.com.br/pages/social`;
+      link = `https://linktr.ee/igreja.viva`;
+    }
+
+    if (id === "igreja-viva-banners-celula") {
+      //link = `https://recepcao.igrejavivaemcelulas.com.br/pages/celulas`;
+      link = `https://linktr.ee/igreja.viva`;
+    }
+    if (id === "igreja-viva-campanha-caixa-dogao") {
+      //link = `https://recepcao.igrejavivaemcelulas.com.br/pages/celulas`;
+      link = `https://linktr.ee/igreja.viva`;
+    }
+    return link;
   } catch (error) {
     return link;
   }
