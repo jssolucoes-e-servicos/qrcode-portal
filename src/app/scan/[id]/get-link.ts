58db2c351a4cefd9deacadccb8f374b052d760ab
JS Soluções e Serviços <49538838+jssolucoes-e-servicos@users.noreply.github.com>
2025-10-05 15:04:16 -0300

    FIX changelinks

--- src/app/scan/[id]/get-link.ts
+++ src/app/scan/[id]/get-link.ts
@@ -4,26 +4,24 @@ export async function getLink(id: string) {
   let link = null;
   try {
     if (id === "igreja-viva-banners-igreja") {
-      //link = `https://recepcao.igrejavivaemcelulas.com.br/pages/a-igreja`;
-      link = `https://linktr.ee/igreja.viva`;
+      link = `https://www.igrejavivaemcelulas.com.br/igreja`;
+      //link = `https://linktr.ee/igreja.viva`;
     }
 
     if (id === "igreja-viva-banners-viva") {
-      //link = `https://recepcao.igrejavivaemcelulas.com.br/pages/social`;
-      link = `https://linktr.ee/igreja.viva`;
+      link = `https://www.igrejavivaemcelulas.com.br/quadra-viva`;
+      //link = `https://linktr.ee/igreja.viva`;
     }
 
     if (id === "igreja-viva-banners-celula") {
-      //link = `https://recepcao.igrejavivaemcelulas.com.br/pages/celulas`;
-      link = `https://linktr.ee/igreja.viva`;
+      link = `https://www.igrejavivaemcelulas.com.br/celulas`;
+      //link = `https://linktr.ee/igreja.viva`;
     }
     if (id === "igreja-viva-campanha-caixa-dogao") {
-      //link = `https://recepcao.igrejavivaemcelulas.com.br/pages/celulas`;
-      link = `https://linktr.ee/igreja.viva`;
+      link = `https://www.igrejavivaemcelulas.com.br/dogao-do-pastor`;
+      //link = `https://linktr.ee/igreja.viva`;
     }
-    if (id === "jantar-maricato-do-pujol") {
-      link = `https://docs.google.com/forms/d/e/1FAIpQLSfJ2NLWjfwrSYNgtZjOBDDCs9wjiOv-I057IltqYcCaxNYcNQ/viewform`;
-    }
+   
     return link;
   } catch (error) {
     return link;
